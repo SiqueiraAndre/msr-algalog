b019f0d1e68c1536d2984de276c9e45c04d388f5
Andre Siqueira <siqueira.andre@gmail.com>
2022-03-13 12:01:46 -0300

    rename dir

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,4 +1,4 @@
-package com.teiaeducacional.teialog.api.controller;
+package com.algaworks.algalog.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/algaworks/algalog/TeialogApiApplicationTests.java
+++ src/test/java/com/algaworks/algalog/TeialogApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.teiaeducacional.teialog;
+package com.algaworks.algalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
