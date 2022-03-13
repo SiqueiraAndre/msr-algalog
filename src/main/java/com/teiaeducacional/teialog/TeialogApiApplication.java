0be0ca9b17b585b8680c359d8003032f8970e7d1
Andre Siqueira <siqueira.andre@gmail.com>
2022-03-13 11:41:56 -0300

    Commit initial

---
+++ src/main/java/com/teiaeducacional/teialog/TeialogApiApplication.java
@@ -0,0 +1,13 @@
+package com.teiaeducacional.teialog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeialogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeialogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teiaeducacional/teialog/TeialogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teiaeducacional.teialog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeialogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
