675bed1721244321c0b5d566b82d1719cd957db7
Andre Siqueira <siqueira.andre@gmail.com>
2022-03-14 20:23:16 -0300

    aula 2 - validando com Bean validation

--- src/main/java/com/algaworks/algalog/AlgalogApiApplication.java
+++ src/main/java/com/algaworks/algalog/AlgalogApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TeialogApiApplication {
+public class AlgalogApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TeialogApiApplication.class, args);
+        SpringApplication.run(AlgalogApiApplication.class, args);
     }
 
 }
